0a13b2beb8c893a939e4e417abe4794829121cf3
“kukim” <kukim.dev@gmail.com>
2022-03-01 22:34:58 +0900

    init: spring boot only core & gradle

---
+++ src/test/java/kukim/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kukim.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
