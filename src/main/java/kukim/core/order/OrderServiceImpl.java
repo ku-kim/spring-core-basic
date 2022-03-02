96bce274a6165cb813985347459f36b27a8f4690
“kukim” <kukim.dev@gmail.com>
2022-03-02 09:10:15 +0900

    case DIP, OCP 위반 사례: Rate할인 기능 메인 코드 적용할 때 DIP, OCp 위반

--- src/main/java/kukim/core/order/OrderServiceImpl.java
+++ src/main/java/kukim/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package kukim.core.order;
 
 import kukim.core.discount.DiscountPolicy;
 import kukim.core.discount.FixDiscountPolicy;
+import kukim.core.discount.RateDiscountPolicy;
 import kukim.core.member.Member;
 import kukim.core.member.MemberRepository;
 import kukim.core.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import kukim.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
