d02aeacfcc3f0064978fcbc06b3f2738b103fa5a
“kukim” <kukim.dev@gmail.com>
2022-03-02 09:50:38 +0900

    refactor: AppConfig
    - memberRepository, discountPolicy 메서드 추출

--- src/main/java/kukim/core/AppConfig.java
+++ src/main/java/kukim/core/AppConfig.java
@@ -12,12 +12,19 @@ import kukim.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
