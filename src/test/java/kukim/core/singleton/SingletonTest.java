b3d5f31159a6c12bbd4a00105b0b567f5c3d27c4
“kukim” <kukim.dev@gmail.com>
2022-03-04 10:56:33 +0900

    싱글톤: 스프링없이 순수 DI 컨테이너로 생성한 객체(싱글톤X)
    - 메서드 호출에 따라 객체가 계속 생성

---
+++ src/test/java/kukim/core/singleton/SingletonTest.java
@@ -0,0 +1,25 @@
+package kukim.core.singleton;
+
+import kukim.core.AppConfig;
+import kukim.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SingletonTest {
+
+    @Test
+    @DisplayName("스프링없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        assertThat(memberService1).isNotEqualTo(memberService2);
+    }
+
+}
