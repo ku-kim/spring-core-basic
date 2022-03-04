ed61959ab8f43f40832d2c223c0d2147763caf7c
“kukim” <kukim.dev@gmail.com>
2022-03-04 14:28:28 +0900

    컴포넌트 : 컴포넌트 스캔과 의존관계 자동 주입
    - AutoAppConfig에서 @Configuration와 @ComponentScan을 활용해서 스프링 컨테이너로 만들 클래스들의 위에 @Component를 설정하여 Scan하도록 만든다.
    - 스프링 컨테이너의 의존관계 주입은 @Autowired를 통해 자동으로 주입할 수 있다.

--- src/main/java/kukim/core/discount/RateDiscountPolicy.java
+++ src/main/java/kukim/core/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package kukim.core.discount;
 
 import kukim.core.member.Grade;
 import kukim.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountRateAmount = 10;
