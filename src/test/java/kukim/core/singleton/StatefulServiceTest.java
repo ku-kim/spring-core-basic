ae2c5d3e89d98aec159885f639640f22c031e83c
“kukim” <kukim.dev@gmail.com>
2022-03-04 11:04:54 +0900

    싱글톤: 주의사항 - 싱글톤이나 스프링 빈은 항상 무상태(stateless)로 설계하여 공유 필드를 조심해야한다

---
+++ src/test/java/kukim/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,36 @@
+package kukim.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //ThreadA: A사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+        //ThreadB: B사용자 20000원 주문
+        statefulService2.order("userB", 20000);
+
+        // 1번은 10000원이어야 하지만 20000원이 됨
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
