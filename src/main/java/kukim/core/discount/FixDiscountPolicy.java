24d4386f27a12460f3db194b2ba6b6c7a84168c0
“kukim” <kukim.dev@gmail.com>
2022-03-07 11:25:41 +0900

    조회 빈이 2개일 때 자동 조회 실패 : UnsatisfiedDependencyException
    - DiscountPolicy 인터페이스에 FixDiscountPolicy, RateDisCountPolicy 두개가 컴포넌트로 등록되면 @Autowired 조회할 때 예외가 발생됨 (테스트 코드 실행해보면 AutoAppConfigTest의 basicScan 실패하는 것 확인 가능)

--- src/main/java/kukim/core/discount/FixDiscountPolicy.java
+++ src/main/java/kukim/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package kukim.core.discount;
 
 import kukim.core.member.Grade;
 import kukim.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
     private int discountFixAmount = 1000;
 
