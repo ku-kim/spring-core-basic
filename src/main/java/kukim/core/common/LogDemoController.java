337f7f605cedb0780abbda8e73c2a3278a89b82a
“kukim” <kukim.dev@gmail.com>
2022-03-08 15:52:14 +0900

    웹 스코프 : Request 스코프 + 프록시
    - @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS) 추가하여 손쉽게 requet 스코프를 싱글톤처럼 보이게 사용 가능
    - CGLIB를 사용해 가짜 프록시 객체 만들어 주입하고, 실제 요청오면 이때 가짜 객체 내부 실제 빈 요청하는 위임 로직을 통해 실제 빈 을 사용하도록 만듬
    - Provider, 프록시 핵심아이디어 모두 의존성 주입은 가짜로하고 객체 조회할 때 진짜를 전달해주는 '지연처리' 방법이다
    - 주의 : 프록시는 싱글톤처럼 보이지만 다르게 동작하는 이해, 무분별한 사용 유지보수 어려우니 꼭 필요한 곳에서만 사용

--- src/main/java/kukim/core/common/LogDemoController.java
+++ src/main/java/kukim/core/common/LogDemoController.java
@@ -1,7 +1,6 @@
 package kukim.core.common;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,13 +11,14 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
--- src/main/java/kukim/core/common/LogDemoService.java
+++ src/main/java/kukim/core/common/LogDemoService.java
@@ -1,17 +1,15 @@
 package kukim.core.common;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
